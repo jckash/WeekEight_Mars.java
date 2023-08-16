6af3f3e0a9ad5f189582c06dcd0198f55e466e20
Jennifer Kash <jenniferkash3@gmail.com>
2023-08-16 12:32:19 -0400

    Week8

---
+++ GuessingGame.java
@@ -0,0 +1,13 @@
+public class GuessingGame {
+    public static void main(String[] args) {
+
+
+          public GuessingGame() {
+            System.out.println("Welcome to the guessing game!");
+
+        }
+
+
+    }
+
+}
---
+++ Mars.java
@@ -0,0 +1,50 @@
+public class Mars {
+    public static void main(String[] args) throws InterruptedException {
+        String colonyName = "File Invaders";
+        int shipPopulation = 300;
+        double meals = 4000.00;
+        boolean landing = true;
+
+        meals -= (shipPopulation * 0.75) * 2;
+        System.out.println(meals);
+        meals += (meals * 0.5);
+        shipPopulation += 5;
+        System.out.println(shipPopulation);
+
+        String landingLocation = "The Plain";
+        if (landingLocation.equalsIgnoreCase("The Plain")) {
+            System.out.println("Bbzzz Landing on the Plain");
+        } else {
+            System.out.println("ERROR!!! Flight plan already set. Landing on the Plain");
+        }
+        landing = landingCheck(100);
+    }
+
+
+    public static boolean landingCheck(int minutesLeft) throws InterruptedException {
+        for (int minute = 0; minute <= minutesLeft; minute++) {
+            if ((minute % 2) == 0) {
+                System.out.println("Right");
+            } else if ((minute % 3) == 0) {
+                System.out.println("Left");
+            } else if (((minute % 2) == 0) && ((minute % 3) == 0)) {
+                System.out.println("Keep Center");
+            } else {
+                System.out.println("Calculating");
+            }
+            Thread.sleep(250);
+        }
+        System.out.println("Landed");
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
